5a9dfcdbcde107db65d62d71e21fe282619ee0c0
kimdaebeom <68265609+kimdaebeom@users.noreply.github.com>
2021-06-29 16:41:49 +0900

    Add files via upload

---
+++ opencv_cpp/CMakeFiles/OpenCV_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenCV_test.dir/main.cpp.o"
+  "OpenCV_test.pdb"
+  "OpenCV_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCV_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
